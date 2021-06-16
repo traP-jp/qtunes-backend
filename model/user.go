fdccb908480e146dd7b62f12fadb64837657fe0c
Ras96 <asymptote.k.k@gmail.com>
2021-06-16 11:59:07 +0900

    :bug: 残骸消した

--- model/user.go
+++ model/user.go
@@ -11,7 +11,7 @@ var baseURL, _ = url.Parse("https://q.trap.jp/api/v3")
 
 type User struct {
 	ID        string    `json:"id"  db:"id"`
-	TraqID    string    `json:"traq_id"  db:"traq_id"`
+	Name      string    `json:"name"  db:"name"`
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
 }
 
@@ -26,7 +26,7 @@ func GetUsers(ctx context.Context) ([]*User, error) {
 }
 
 func CreateUser(ctx context.Context, user *User) error {
-	_, err := db.Query("INSERT INTO users (id, traq_id) VALUES (?, ?)", user.ID, user.TraqID)
+	_, err := db.Query("INSERT INTO users (id, name) VALUES (?, ?)", user.ID, user.Name)
 	if err != nil {
 		return err
 	}
