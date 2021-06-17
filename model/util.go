9a414ec84ee5e0c59fe7d44e31033f62caa36319
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-06-17 22:09:26 +0900

    BaseUrlの大文字化

--- model/util.go
+++ model/util.go
@@ -2,5 +2,5 @@ package model
 
 import "net/url"
 
-var baseUrl,_=url.Parse("https://q.trap.jp/api/v3")
+var BaseUrl,_=url.Parse("https://q.trap.jp/api/v3")
 const soundChannelId="8bd9e07a-2c6a-49e6-9961-4f88e83b4918"
