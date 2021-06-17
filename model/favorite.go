07ea63dda6a55c1b4638d78fb5317f029000ff1a
Ras96 <asymptote.k.k@gmail.com>
2021-06-18 02:40:14 +0900

    :recycle:       リファクタリング

--- model/favorite.go
+++ model/favorite.go
@@ -7,17 +7,17 @@ import (
 
 type FavoriteCount struct {
 	ComposerID string `db:"composer_id"`
-	Count      uint8  `db:"count"`
+	Count      uint32 `db:"count"`
 }
 type MyFavorites []string
 
-func getFavoriteCounts(ctx context.Context) (map[string]uint8, error) {
-	favCnt := make([]*FavoriteCount, 0)
+func getFavoriteCounts(ctx context.Context) (map[string]uint32, error) {
+	var favCnt []*FavoriteCount
 	err := db.SelectContext(ctx, &favCnt, "SELECT composer_id, COUNT( composer_id ) as count FROM favorites GROUP BY composer_id")
 	if err != nil {
 		return nil, fmt.Errorf("Failed to get favorite counts: %w", err)
 	}
-	res := make(map[string]uint8)
+	res := make(map[string]uint32)
 	for _, v := range favCnt {
 		res[v.ComposerID] = v.Count
 	}
@@ -26,7 +26,7 @@ func getFavoriteCounts(ctx context.Context) (map[string]uint8, error) {
 }
 
 func getMyFavorites(ctx context.Context, userID string) (map[string]bool, error) {
-	myFavorites := make(MyFavorites, 0)
+	var myFavorites MyFavorites
 	err := db.SelectContext(ctx, &myFavorites, "SELECT sound_id FROM favorites WHERE user_id = ?", userID)
 	if err != nil {
 		return nil, fmt.Errorf("Failed to get Your Favorite Files: %w", err)
