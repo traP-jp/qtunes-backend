1744cfeb7a749ab4efab02f716712e870b31bba6
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-06-18 23:50:02 +0900

    domainにComposers構造体を追加

--- domain/file.go
+++ domain/file.go
@@ -9,3 +9,9 @@ type File struct {
 	IsFavoriteByMe bool      `json:"is_favorite_by_me"`
 	CreatedAt      time.Time `json:"created_at"`
 }
+
+type Composers struct {
+	ID        string `json:"id"`
+	Name      string `json:"name"`
+	PostCount int    `json:"post_count"`
+}
