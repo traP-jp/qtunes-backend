174366806c4aaaf9c13a8739dfea0776c09c6e8a
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-06-19 00:00:12 +0900

    SoundChannelIdの頭を大文字にした(golangciで変えろっていわれた)

--- model/util.go
+++ model/util.go
@@ -4,4 +4,4 @@ import "net/url"
 
 var BaseUrl, _ = url.Parse("https://q.trap.jp/api/v3")
 
-const soundChannelId = "8bd9e07a-2c6a-49e6-9961-4f88e83b4918"
+const SoundChannelId = "8bd9e07a-2c6a-49e6-9961-4f88e83b4918"
