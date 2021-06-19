e7f82827a6b433df51b18f2e12d67aa632342669
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-06-19 18:53:06 +0900

    Offsetと凍結ユーザーを含めてたくさん取得できるようにした

--- model/composer.go
+++ model/composer.go
@@ -12,29 +12,33 @@ import (
 
 func GetComposers(ctx context.Context, accessToken string) ([]*domain.Composer, error) {
 	client, auth := newClient(accessToken)
-	users, res, err := client.UserApi.GetUsers(auth, nil)
+	users, res, err := client.UserApi.GetUsers(auth,&traq.UserApiGetUsersOpts{IncludeSuspended: optional.NewBool(true)})
 	if err != nil {
 		return nil, err
 	}
 	if res.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("failed in HTTP request:(status:%d %s)", res.StatusCode, res.Status)
 	}
-
-	files, res, err := client.FileApi.GetFiles(auth, &traq.FileApiGetFilesOpts{
-		ChannelId: optional.NewInterface(SoundChannelId),
-		Limit:     optional.NewInt32(200),
-	})
-	if err != nil {
-		return nil, err
-	}
-	if res.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("failed in HTTP request:(status:%d %s)", res.StatusCode, res.Status)
-	}
-
 	postCountByUser := make(map[string]int)
-	for _, v := range files {
-		if strings.HasPrefix(v.Mime, "audio") {
-			postCountByUser[*v.UploaderId]++
+	for i:=0;;i+=200{
+		files, res, err := client.FileApi.GetFiles(auth, &traq.FileApiGetFilesOpts{
+			ChannelId: optional.NewInterface(SoundChannelId),
+			Limit:     optional.NewInt32(200),
+			Offset: 	optional.NewInt32(int32(i)),
+		})
+		if err != nil {
+			return nil, err
+		}
+		if res.StatusCode != http.StatusOK {
+			return nil, fmt.Errorf("failed in HTTP request:(status:%d %s)", res.StatusCode, res.Status)
+		}
+		if len(files)==0 {
+			break
+		}
+		for _, v := range files {
+			if strings.HasPrefix(v.Mime, "audio") {
+				postCountByUser[*v.UploaderId]++
+			}
 		}
 	}
 	composers := make([]*domain.Composer, 0, len(users))
