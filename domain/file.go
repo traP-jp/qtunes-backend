5dbfb8db19e829d7ab231b048f009bc7b0415789
Ras96 <asymptote.k.k@gmail.com>
2021-06-24 01:31:54 +0900

    💥: テーブル追加
    filesを追加
    domain.Fileに`MessageID`を追加
    
    BREAKING CHANGE:
    domain.Fileの要素追加

--- domain/file.go
+++ domain/file.go
@@ -7,6 +7,7 @@ type File struct {
 	Title          string    `json:"title"`
 	ComposerID     string    `json:"composer_id"`
 	ComposerName   string    `json:"composer_name"`
+	MessageID      string    `json:"message_id"`
 	FavoriteCount  uint32    `json:"favorite_count"`
 	IsFavoriteByMe bool      `json:"is_favorite_by_me"`
 	CreatedAt      time.Time `json:"created_at"`
