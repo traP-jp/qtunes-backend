f57f8e65ccdd595112b73b20dd38290aa234bb7f
Ras96 <asymptote.k.k@gmail.com>
2021-07-03 22:43:50 +0900

    🗑️  使っていないタグを消去

--- domain/composer.go
+++ domain/composer.go
@@ -3,8 +3,8 @@ package domain
 import "time"
 
 type Composer struct {
-	ID        string    `json:"id"`
-	Name      string    `json:"name"`
-	PostCount int       `json:"post_count"`
-	UpdatedAt time.Time `json:"-"`
+	ID        string `json:"id"`
+	Name      string `json:"name"`
+	PostCount int    `json:"post_count"`
+	UpdatedAt time.Time
 }
