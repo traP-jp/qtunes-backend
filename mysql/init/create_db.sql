f9cc5a05eebd61658544d77c1cc249efa7335442
ras0q <ras0q@users.noreply.github.com>
2025-02-04 14:34:53 +0900

    upsert file

--- mysql/init/create_db.sql
+++ mysql/init/create_db.sql
@@ -16,10 +16,11 @@ CREATE TABLE IF NOT EXISTS `favorites` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `files` (
-  `id` char(36) NOT NULL,
+  `id` char(36) PRIMARY KEY,
   `title` text NOT NULL,
   `composer_id` char(36) NOT NULL,
   `composer_name` varchar(32) NOT NULL,
   `message_id` char(36) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
